f859ec00f607b1ed1d3df16ea0b5dafca24d757d
jenajeong <skg4660@gmail.com>
2026-04-29 18:34:20 +0900

    [FIX] management command 실행 시 불필요하게 Embedder/OpenAI 초기화하지 않도록 예외 처리

--- llm_rag_matching_api/search/apps.py
+++ llm_rag_matching_api/search/apps.py
@@ -1,8 +1,23 @@
 from django.apps import AppConfig
+import sys
 
 class SearchConfig(AppConfig):
     name = 'search'
 
     def ready(self):
+        management_commands_without_search_runtime = {
+            "build_rag_index",
+            "collect_rag_sources",
+            "clear_search_result_cache",
+            "collectstatic",
+            "filter_rag_data",
+            "makemigrations",
+            "migrate",
+            "shell",
+            "test",
+        }
+        if len(sys.argv) > 1 and sys.argv[1] in management_commands_without_search_runtime:
+            return
+
         from .engine.embedder import Embedder
         Embedder()
