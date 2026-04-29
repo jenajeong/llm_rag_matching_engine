10007c7bbe5a1e52f7d5e9d40a5e31deee2c0db1
jenajeong <skg4660@gmail.com>
2026-04-29 11:32:27 +0900

    [FIX] report의 url 연결

--- llm_rag_matching_api/config/urls.py
+++ llm_rag_matching_api/config/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/search/', include('search.urls')),
+    path('api/v1/report/', include('report.urls')),
 ]
