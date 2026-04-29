9e2af9fce6c7a2f208d090dde93239ab29f8c907
jenajeong <skg4660@gmail.com>
2026-04-29 13:18:29 +0900

    [FIX] LOGGING 설정 추가

--- llm_rag_matching_api/config/settings.py
+++ llm_rag_matching_api/config/settings.py
@@ -131,3 +131,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
