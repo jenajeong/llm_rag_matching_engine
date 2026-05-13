4dfd10429d4eb928c3074115336a653fa17c0cae
jenajeong <skg4660@gmail.com>
2026-05-13 18:08:05 +0900

    [FIX] 메인 화면에 대한 URL

--- llm_rag_matching_api/config/urls.py
+++ llm_rag_matching_api/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from search.views import recommend
+
 urlpatterns = [
+    path('', recommend, name='search-home'),
     path('admin/', admin.site.urls),
     path('api/v1/search/', include('search.urls')),
     path('api/v1/report/', include('report.urls')),
